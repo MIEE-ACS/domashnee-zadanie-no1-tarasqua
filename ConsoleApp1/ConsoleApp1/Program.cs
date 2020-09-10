a325c38136f37f2a19cecf929832cb003d5e7887
Tarasov Kirill <tarasqua228@gmail.com>
2020-09-10 17:44:20 +0300

    Задание выполнил!
    В ходе решения возник вопрос насчет того, как сделать так, чтобы консоль не закрывалась и результат был виден. Решил сделать цикл с постусловием, и предоставить выбор пользователю - закрыть или продолжить. Так же в домашнем задании было сказано, что программа должна прекращаться, если возникает ошибка. Можно ли считать, что она закрылась, или нужно было закрыть ее с помощью "break"?

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class functionCalculator
+    {
+        static void Main(string[] args)
+        {
+            do //цикл с постусловием
+            {
+
+                double x, y; //объявляем переменные
+
+                try //проверка на ошибку 
+                {
+                    Console.WriteLine("Введите значение x:"); //поясняем, что нужно ввести пользователю
+                    x = double.Parse(Console.ReadLine()); //вводим значение x
+
+                    y = 3 * Math.Pow(x, 6) - 6 * Math.Pow(x, 2) - 7; //вычисления 
+
+                    Console.WriteLine($"Значение функции y = {Math.Round(y, 3)}"); //вывод данных
+
+                }
+                catch
+                {
+                    Console.WriteLine("Внимание! Неправильный ввод данных!"); //оповещение при ошибке 
+                }
+
+                Console.WriteLine("Для завершения программы введите 'exit'. Чтобы продолжить, нажмите любую клавишу."); //диалоговое окно для выхода из программы
+                //(сделал так, чтобы можно было увидеть результат и/или, при желании, продолжить вычисления) 
+
+            }
+            while (Console.ReadLine() != "exit"); //условие
+        }
+    }
+}
